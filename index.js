7fe4cd45910c3e6ed12c2f4c20fc188b6dcdc03b
Kurkov11 <kurkovsw@icloud.com>
2024-01-01 17:25:25 +0100

    Add a functionality that checks for active element on body click and sets the border for an input

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 document.getElementById('html_body').onclick = function(){
     console.log('body clicked');
-    
+    let inputList = document.getElementsByClassName('card-input');
+    for(let i = 0; i < inputList.length; i++){
+        if(document.activeElement == inputList[i]){
+            inputList[i].style.border = '1px solid black';
+        }
+    }
 }
