c9c0df3fe95d8a92b22ff4c3b88cdebcd9afa797
Kurkov11 <kurkovsw@icloud.com>
2024-01-01 17:28:44 +0100

    When clicked element turns out to be active, set all the other input borders to normal style

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ document.getElementById('html_body').onclick = function(){
     for(let i = 0; i < inputList.length; i++){
         if(document.activeElement == inputList[i]){
             inputList[i].style.border = '1px solid black';
+            for(let j = 0; j < inputList.length; j++){
+                if(j != i){
+                    inputList[j].style.border = '1px solid hsla(279, 6%, 55%, 0.3)';
+                }
+            }
         }
     }
 }
