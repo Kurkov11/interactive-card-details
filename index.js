2807be66d70a0aa5bfb9d2b6335bf934f05b3d6d
Kurkov11 <kurkovsw@icloud.com>
2024-01-01 17:39:46 +0100

    Add anyInputActive variable and add functionality that checks if on body click any of the inputs is active. If it is not the border of every input reverts back to normal

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 document.getElementById('html_body').onclick = function(){
     console.log('body clicked');
     let inputList = document.getElementsByClassName('card-input');
+    let anyInputActive = false;
+
     for(let i = 0; i < inputList.length; i++){
         if(document.activeElement == inputList[i]){
             inputList[i].style.border = '1px solid black';
@@ -9,6 +11,13 @@ document.getElementById('html_body').onclick = function(){
                     inputList[j].style.border = '1px solid hsla(279, 6%, 55%, 0.3)';
                 }
             }
+            anyInputActive = true;
+        }
+    }
+
+    if(!anyInputActive){
+        for(let j = 0; j < inputList.length; j++){
+            inputList[j].style.border = '1px solid hsla(279, 6%, 55%, 0.3)';
         }
     }
 }
