a6206ee20e3689773e2595ecc6c55f43fa0af409
Kurkov11 <kurkovsw@icloud.com>
2024-01-02 08:56:45 +0100

    Replace borders with outlines for every input to avoid the jumping of content when setting border to 0 in JS

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ document.getElementById('html_body').onclick = function(){
 
     for(let i = 0; i < inputList.length; i++){
         if(document.activeElement == inputList[i]){
-            inputList[i].style.border = '0';
+            inputList[i].style.outline = '0';
             gradientList[i].style.backgroundImage = 'linear-gradient(hsl(249, 99%, 64%), hsl(278, 94%, 30%))';
             for(let j = 0; j < inputList.length; j++){
                 if(j != i){
-                    inputList[j].style.border = '1px solid hsla(279, 6%, 55%, 0.3)';
+                    inputList[j].style.outline = '1px solid hsla(279, 6%, 55%, 0.3)';
                     gradientList[j].style.backgroundImage = 'none';
                 }
             }
@@ -20,7 +20,7 @@ document.getElementById('html_body').onclick = function(){
 
     if(!anyInputActive){
         for(let i = 0; i < inputList.length; i++){
-            inputList[i].style.border = '1px solid hsla(279, 6%, 55%, 0.3)';
+            inputList[i].style.outline = '1px solid hsla(279, 6%, 55%, 0.3)';
             gradientList[i].style.backgroundImage = 'none';
         }
     }
